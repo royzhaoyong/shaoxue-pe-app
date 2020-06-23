c811841d6eea5c1b763dd7c69dba25060d2b25ea
reactnative <royzhaoyong@hotmail.com>
2020-06-22 18:59:31 -0700

    Updates

---
+++ precache-manifest.438712be04956661dde6ed4cb268fb6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b24573cbc78dcf8c18a94d6634d1731",
+    "url": "/shaoxue-pe-app/index.html"
+  },
+  {
+    "revision": "6427e040d7c07419c3b3",
+    "url": "/shaoxue-pe-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14d4db3bee5c7f6729c7",
+    "url": "/shaoxue-pe-app/static/js/2.6df59cfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shaoxue-pe-app/static/js/2.6df59cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6427e040d7c07419c3b3",
+    "url": "/shaoxue-pe-app/static/js/main.68ea0a9a.chunk.js"
+  },
+  {
+    "revision": "4e1a589cef1749c6b683",
+    "url": "/shaoxue-pe-app/static/js/runtime-main.2dfbec5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shaoxue-pe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
